9ddcf9022fd3cda523cb838d42c7f1df2082f9fa
Takhsela <takhsela86@gmail.com>
2021-10-12 14:31:37 -0400

    Update Xpath_for_table.java
    getting text from web table

--- src/selenium_selectors/Xpath_for_table.java
+++ src/selenium_selectors/Xpath_for_table.java
@@ -1,7 +1,9 @@
 package selenium_selectors;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Xpath_for_table {
@@ -10,12 +12,20 @@ public class Xpath_for_table {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\Takhsela\\Downloads\\chromedriver_win32\\chromedriver.exe");	 
 		WebDriver driver = new ChromeDriver();	 
-		String url_name = "http://demo.guru99.com/test/write-xpath-table.html";
-        driver.get(url_name);
+	//	String url_name = "http://demo.guru99.com/test/write-xpath-table.html";
+   //     driver.get(url_name);
+     //   driver.manage().window().maximize();
+     //   String innerText = driver.findElement(By.xpath("(//td[\"fourth cell\"])[4]")).getText(); 		
+      //  System.out.println(innerText); 
+   //     driver.quit();
+        String url1_name="http://demo.guru99.com/test/newtours/";
+        driver.get(url1_name);
         driver.manage().window().maximize();
-        String innerText = driver.findElement(By.xpath("(//td[\"fourth cell\"])[4]")).getText(); 		
-        System.out.println(innerText); 
-        driver.quit();
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        WebElement move_to=driver.findElement(By.xpath("//img[@src=\"images/featured_destination.gif\"]"));
+        js.executeScript("arguments[0].scrollIntoView();",  move_to);
+        String print_text = driver.findElement(By.xpath("(//font[@face=\"Arial, Helvetica, sans-serif\"])[6]")).getText();
+        System.out.println(print_text);
 	}
 
 }
