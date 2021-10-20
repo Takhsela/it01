796cd498160453ee50dac7061a9309d777272ca5
Takhsela <takhsela86@gmail.com>
2021-10-20 13:16:45 -0400

    Update Max_from_column_table.java
    fixed the errors

--- src/selenium_selectors/Max_from_column_table.java
+++ src/selenium_selectors/Max_from_column_table.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Max_from_column_table {
@@ -21,14 +22,14 @@ public class Max_from_column_table {
 			     double m=0,r=0;
 				 
 			       //No. of Columns
-			        List  col = wd.findElements(By.xpath(".//*[@id='leftcontainer']/table/thead/tr/th"));
+			        List <WebElement> col = wd.findElements(By.xpath(".//*[@id='leftcontainer']/table/thead/tr/th"));
 			        System.out.println("Total No of columns are : " +col.size());
 			        //No.of rows
-			        List  rows = wd.findElements(By.xpath (".//*[@id='leftcontainer']/table/tbody/tr/td[1]"));
+			        List <WebElement> rows = wd.findElements(By.xpath (".//*[@id='leftcontainer']/table/tbody/tr/td[1]"));
 			        System.out.println("Total No of rows are : " + rows.size());
 			        for (int i =1;i<rows.size();i++)
 			        {    
-			            max= wd.findElement(By.xpath("html/body/div[1]/div[5]/table/tbody/tr[" + (i+1)+ "]/td[4]")).getText();
+			            max= wd.findElement(By.xpath("//*[@id=\"leftcontainer\"]/table/tbody/tr["+ (i+1)+"]/td[4]")).getText();
 			            NumberFormat f =NumberFormat.getNumberInstance(); 
 			            Number num = f.parse(max);
 			            max = num.toString();
